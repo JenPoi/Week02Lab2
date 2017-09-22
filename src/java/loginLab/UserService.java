e565d518d8a051a5204eb1dc1ab3540bb38996bf
617702 <617702@ELF6IT215744.ACDM.DS.SAIT.CA>
2017-09-22 13:32:50 -0600

    New Project Creation Log out link doesn't work

---
+++ src/java/loginLab/UserService.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loginLab;
+
+/**
+ *
+ * @author 617702
+ */
+public class UserService
+{
+    public UserService()
+            {
+                
+            }
+    boolean login(String username, String password)
+    {
+        if(password.equalsIgnoreCase("password") && username.equalsIgnoreCase("adam") || username.equalsIgnoreCase("betty"))
+                return true;
+        else
+            return false;
+    }
+}
